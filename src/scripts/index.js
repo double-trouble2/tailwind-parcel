b7bb3de3bc74ee3b8bafcc5ae2e2be8243b38e5c
Islombek Gofurov <barr.mrjohn@gmail.com>
2021-01-08 03:53:17 +0500

    Build to publish v1.3.52

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,15 +2,14 @@ import '../styles/styles.css';
 import Glide from './glide.min.js';
 
 // Our Partners or clients slider
-new Glide('.partners', {
+new Glide('.partners-carousel', {
   type: 'carousel',
   startAt: 0,
   autoplay: 3000,
   hoverpause: true,
-  width: 380,
-  gap: 50,
+  gap: 25,
   // animationTimingFunc: ease,
-  perView: 6,
+  perView: 5,
   breakpoints: {
     800: {
       perView: 4
@@ -22,24 +21,61 @@ new Glide('.partners', {
 }).mount();
 
 // 
-new Glide('.news', {
-  type: 'carousel',
+new Glide('.news-slider', {
+  type: 'slider',
   startAt: 0,
   autoplay: 3000,
   hoverpause: true,
-  animationDuration: 600,
-  animationTimingFunc: 'linear',
-  focusAt: 0,
-  width: 280,
-  gap: 33,
-  // animationTimingFunc: ease,
+  transitionType: 'slide',
+  animationDuration: 500,
+  animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+  // animationTimingFunc: linear,
+  bound: true,
+  rewind: false,
+  width: 225,
+  gap: 18,
   perView: 3,
   breakpoints: {
-    800: {
-      perView: 3
+    768: {
+      peek: 80,
+      perView: 1
     },
-    600: {
-      perView: 2
+    568: {
+      peek: 50,
+      perView: 1
     }
   }
 }).mount();
+
+new Glide('.tariffs-slider', {
+  type: 'slider',
+  startAt: 0,
+  autoplay: 3000,
+  wrapperWidth: 300,
+  hoverpause: true,
+  transitionType: 'slide',
+  animationDuration: 500,
+  animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+  // animationTimingFunc: linear,
+  bound: true,
+  rewind: false,
+  perView: 1
+}).mount();
+
+// Slowly scroling
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
